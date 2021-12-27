577c9464d7d4014dbaf516a19a7acae34c753f4e
phat <luongphat.nguyen@shopee.com>
2021-12-27 15:43:12 +0700

    create container deploy script

---
+++ container/config/webpack.prod.js
@@ -0,0 +1,23 @@
+const { merge } = require("webpack-merge");
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+const packageJson = require("../package.json");
+const commonConf = require("./webpack.common");
+const domain = process.env.PRODUCTION_DOMAIN;
+
+const prodConf = {
+  mode: "production",
+  output: {
+    filename: "[name].[contenthash].js",
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: "container",
+      remotes: {
+        marketing: `marketing@${domain}/marketing/remoteEntry.js`,
+      },
+      shared: packageJson.dependencies,
+    }),
+  ],
+};
+
+module.exports = merge(commonConf, prodConf);
